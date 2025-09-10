f41d180e1b1fea59d9113debf028283992d62022
Rogério Santos <rogeriosantosdev@gmail.com>
2025-09-09 22:47:56 -0300

    first commit

---
+++ src/test/java/com/portifolio/uniguacu/UniguacuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portifolio.uniguacu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniguacuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
